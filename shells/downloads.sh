c7a52fb00cf22476eb1c9843f9fc2143ed5d22ec
Shunta Saito <shunta.saito@gmail.com>
2021-07-22 21:18:28 +0900

    Update downloads.sh

--- shells/downloads.sh
+++ shells/downloads.sh
@@ -14,5 +14,5 @@ mv www.cs.toronto.edu/~vmnih/data ./
 rm -rf www.cs.toronto.edu
 
 mv data/mass_merged/test/map data/mass_merged/test/map_orig
-cd data/mass_merged/test; wget https://www.dropbox.com/s/yk6d4garyz3nm19/multi_test_map.tar.gz?dl=0;
+cd data/mass_merged/test; wget https://www.dropbox.com/s/dzjvjuo3nkcdt81/multi_test_map.tar.gz?dl=0;
 tar zxvf multi_test_map.tar.gz; rm -rf multi_test_map.tar.gz
